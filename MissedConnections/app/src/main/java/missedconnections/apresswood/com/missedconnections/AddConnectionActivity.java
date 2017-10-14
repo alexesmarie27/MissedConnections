153ad458bbdd55c6fbd71c632c3c552ef2caacb7
Alexes Presswood <apresswood@rtsfinancial.com>
2017-10-14 13:30:28 -0500

    Add Location Name to Add Connections Page

--- MissedConnections/app/src/main/java/missedconnections/apresswood/com/missedconnections/AddConnectionActivity.java
+++ MissedConnections/app/src/main/java/missedconnections/apresswood/com/missedconnections/AddConnectionActivity.java
@@ -3,13 +3,27 @@ package missedconnections.apresswood.com.missedconnections;
 import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 
-public class AddConnectionActivity extends Activity implements View.OnClickListener {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.Places;
+
+
+public class AddConnectionActivity extends Activity implements View.OnClickListener,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     protected MenuListener menuListener;
-    private int selectedLocationId;
+    private String selectedLocationId;
+    private Place selectedPlace;
+    private GoogleApiClient googleApiClient;
+    private LocationRequest locationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +32,10 @@ public class AddConnectionActivity extends Activity implements View.OnClickListe
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            selectedLocationId = Integer.parseInt(extras.getString("selectedLocation"));
+            selectedLocationId = extras.getString("selectedLocation");
         }
 
+        buildGoogleApiClient();
         initializeUI();
     }
 
@@ -40,6 +55,24 @@ public class AddConnectionActivity extends Activity implements View.OnClickListe
 
         TextView backButton = findViewById(R.id.back_button);
         backButton.setOnClickListener(this);
+
+        TextView locationText = findViewById(R.id.location_text);
+        getPlaceById(locationText);
+    }
+
+    protected void getPlaceById(final TextView locationText) {
+        Places.GeoDataApi.getPlaceById(googleApiClient, selectedLocationId).setResultCallback(
+                new ResultCallback<PlaceBuffer>() {
+                    @Override
+                    public void onResult(@NonNull PlaceBuffer places) {
+                        if (places.getStatus().isSuccess()) {
+                            selectedPlace = places.get(0);
+                            locationText.setText(selectedPlace.getName());
+                        }
+
+                        places.release();
+                    }
+                });
     }
 
     protected View.OnClickListener getSideMenuListener() {
@@ -53,5 +86,32 @@ public class AddConnectionActivity extends Activity implements View.OnClickListe
                 }
             }
         };
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Places.GEO_DATA_API)
+                .build();
+        googleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(1000);
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult result) {
+        //do nothing for now
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        //do nothing for now
     }
 }
